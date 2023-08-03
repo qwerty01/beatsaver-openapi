526ab13a9f4fa85ac581db980c13bd576f03185c
qwerty01 <qw3rty01@gmail.com>
2023-08-02 23:43:14 -0400

    Fix up generate.sh

--- generate.sh
+++ generate.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+!/bin/bash
 wget https://api.beatsaver.com/docs/swagger.json
 openapi-generator generate -g rust -i swagger.json --skip-validate-spec
 rm swagger.json
+sed -i 's/version = "\([0-9]\+\)\.\([0-9]\+\)"/version = "\1.\2.0"/' Cargo.toml
 cargo fmt
